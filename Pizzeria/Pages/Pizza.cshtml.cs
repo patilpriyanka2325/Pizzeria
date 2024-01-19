9df4598a6a5853b0b7b544e53267cc661328f8dd
patilpriyanka2325 <patilpriyanka2325@gmail.com>
2024-01-19 12:48:32 +0530

    Add project files.

---
+++ Pizzeria/Pages/Pizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pizzeria.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
