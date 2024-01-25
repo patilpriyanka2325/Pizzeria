612db1095a9fc35666e3f70c563f813f87462524
patilpriyanka2325 <patilpriyanka2325@gmail.com>
2024-01-25 15:09:46 +0530

    wip

---
+++ Pizzeria/Models/PizzasModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Pizzeria.Models
+{
+    public class PizzasModel
+    {
+        public string ImageTittle { get; set; }
+        public string PizzaName { get; set;}
+        public float BasePrice { get; set; } = 2;
+        public bool TomatoSauce { get; set;}
+        public bool Cheese { get; set;}
+        public bool PeriPeri { get; set;}
+        public bool Peperoni { get; set;}
+        public bool Mushroom { get; set;}
+        public bool Tuna { get; set;}
+        public bool Chicken { get; set;}
+        public float FinalPrice { get; set;}
+
+    }
+}
